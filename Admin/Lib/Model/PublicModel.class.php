5020c0e80c14f5a463ffe0937596d6fce9795088
jaxma <jaxma@jaxma.com>
2019-01-09 22:19:29 +0800

    .

--- Admin/Lib/Model/PublicModel.class.php
+++ Admin/Lib/Model/PublicModel.class.php
@@ -14,7 +14,7 @@ class PublicModel extends Model {
         }
 		$M = M("admin_user");
 				echo '<pre>';
-				print_r(M('admin_user')->where(1)->select());die;
+				print_r(M('admin_user')->_sql());die;
 		if ( $M->where("`user_name`='" . $datas['username'] . "'")->count()>=1) {
 			$info = $M->where("`user_name`='" . $datas["username"] . "'")->find();		
 			if ($info['password'] == md5($datas['password']) && $info['user_name'] == $datas['username']) {

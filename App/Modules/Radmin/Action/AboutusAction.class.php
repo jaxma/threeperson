16242f96c15898ad0500b6d3a55dd9a1f8a837e0
wcm <13802540915@163.com>
2019-01-12 18:34:15 +0800

    wcm modify 2019-1-12

--- App/Modules/Radmin/Action/AboutusAction.class.php
+++ App/Modules/Radmin/Action/AboutusAction.class.php
@@ -67,95 +67,7 @@ class AboutusAction extends CommonAction {
 
     }
 
-    //摄影分类
-    public function cat(){
-        import('Lib.Action.Team', 'App');
-        $Team = new Team();
-
-        $count =  $this->cat_model->count('id');
-        if ($count > 0) {
-            $this->assign('list', 1);
-        }else{
-            $this->assign('list', '');
-        }
-        $this->display();
-    }
-
-
-
-
-
-    //改变状态---开启或者关闭
-    public function set_status()
-    {
-        if (!IS_AJAX) {
-            return FALSE;
-        }
-        $id = I('id');
-        $status = I('status');
-
-        //  $id=6;
-        //   $status=1;
-        if(empty($id)){
-            $return_result = [
-                'code' => 2,
-                'msg' => 'id获取失败',
-
-            ];
-            $this->ajaxReturn($return_result);
-        }
-        if(empty($status)){
-            $return_result = [
-                'code' => 3,
-                'msg' => '状态获取失败',
-
-            ];
-            $this->ajaxReturn($return_result);
-        }
-        elseif ($status == '1') {
-            $status = 0;
-        } elseif($status == '2'){
-            $status = 1;
-        }
-        $this->cat_model->where(array('id' => $id))->save(['status' => $status]);
-        // $this->model->where(array('category_id'=>$id))->save(['active'=>$status]);
-        $pid_info_one = $this->cat_model->where(array('pid' => $id))->select();
-        if ($pid_info_one) {
-            foreach ($pid_info_one as $v) {
-                $ids = $v['id'];
-                $this->cat_model->where(array('id' => $ids))->save(['status' => $status]);
-                // $this->model->where(array('category_id'=>$ids))->save(['active'=>$status]);
-                $pid_info_two[] = $this->cat_model->where(array('pid' => $ids))->select();
-            }
-
-            if ($pid_info_two) {
-                foreach ($pid_info_two as $table => $row) {
-                    foreach ($row as $col) {
-                        $idss[] = $col['id'];
-                    }
-                }
-                $condition = [
-                    'id' => array('in', $idss)
-                ];
-                $condition_two=[
-                    'category_id' => array('in', $idss)
-                ];
-                $this->cat_model->where($condition)->save(['status' => $status]);
-                // $this->model->where($condition_two)->save(['active'=>$status]);
-            }
-
-        }
-        $return_result = [
-            'code' => 1,
-            'msg' => '获取成功',
-
-        ];
-        $this->ajaxReturn($return_result);
-    }
-
   
-
-
 
     //摄影图片
     //添加产品信息
@@ -181,8 +93,7 @@ class AboutusAction extends CommonAction {
         $detial_title_en = trim(I('post.detial_title_en',''));
         $many_image=I('many_image');
         $image2 = $many_image[0];
-        unset($many_image[0]);
-        $many_images=!empty($many_image)?implode(',',$many_image):'';
+        $many_images = implode(',',$many_image);
         $content = trim(I('post.content',''));
         $content = $this->formateStr($content);
         $content_en = trim(I('post.content_en',''));
@@ -247,7 +158,7 @@ class AboutusAction extends CommonAction {
         $model_name = $this->get_model();
         
         $id = I('post.id');
-        $id_info=M('Photo')->where(array('id' => $id))->find();
+        $id_info=M($model_name)->where(array('id' => $id))->find();
         $old_image=$id_info['image'];
         $image=I('image');
         if(strcmp($old_image,$image)==0){
@@ -258,34 +169,47 @@ class AboutusAction extends CommonAction {
             @unlink($url);
             $image = $image;
         }
-        if(!$image || empty($image)){
-             $this->error('请添加摄影封面图片后,再提交!');
-        }
-        
-        $many_image=I('many_image');
-        $many_images=implode(',',$many_image);
 
-        $news = I('post.disc');
-        $category_id2 = I('post.category_id2');
-        $news = stripslashes($news);
-        $news = preg_replace("/&amp;/", "&", $news);
-        $news = preg_replace("/&quot;/", "\"", $news);
-        $news = preg_replace("/&lt;/", "<", $news);
-        $news = preg_replace("/&gt;/", ">", $news);
-        $isopen = I('post.isopen');
+        $title = trim(I('post.title',''));
+        $title_en = trim(I('post.title_en',''));
+        $category_id1 = I('post.category_id1',0);
+        $category_id2 = I('post.category_id2',0);
+        $isopen=I('post.isopen');
+        $image=I('post.image');
+        $publish_time = I('post.publish_time',date('Y-m-d',time()));
         $sequence = I('post.sequence');
+        $detial_title = trim(I('post.detial_title',''));
+        $detial_title_en = trim(I('post.detial_title_en',''));
+        $many_image=I('many_image');
+        $image2 = $many_image[0];
+        $many_images = implode(',',$many_image);
+        $content = trim(I('post.content',''));
+        $content = $this->formateStr($content);
+        $content_en = trim(I('post.content_en',''));
+        $content_en = $this->formateStr($content_en);
+        if(empty($title)||empty($title_en)||empty($category_id1)||empty($category_id2)||empty($isopen)||empty($image)||empty($detial_title)||empty($detial_title_en)||empty($image2)||empty($content)||empty($content_en)){
+            $this->error('红色带星项目必须填写，请检查后重新提交');
+            exit();
+        }
         $data = array(
+            'title' => $title,
+            'title_en' => $title_en,
+            'title_news' => $detial_title,
+            'title_news_en' => $detial_title_en,
             'image' => $image,
-            'name' => trim(I('post.name')),
-            'cat1' => trim(I('post.category_id1')),
-            'cat2' => trim(I('post.category_id2')),
-            'presents' => I('post.presents'),
-            'news' => $news,
+            'image2' => $image2,
+            'content' => $content,
+            'content_en' => $content_en,
+            'publish_time' => strtotime($publish_time),
             'isopen' => $isopen,
-            'sequence' => $sequence,
             'many_image' => $many_images,
-            'time' => time()
+            'sequence' => $sequence,
+            'time' => time(),
+            'cat1'=> $category_id1,
+            'cat2'=> $category_id2,
         );
+
+        
         $res = D($model_name)->where(array('id' => $id))->save($data);
         if ($res === false) {
             $this->error("操作失败");

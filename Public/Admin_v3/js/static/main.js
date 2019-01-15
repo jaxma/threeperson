a600091285c22f56e367ab22e175257c4c049bd9
zhang <296684750@qq.com>
2019-01-15 20:09:33 +0800

    首页显示样式

--- Public/Admin_v3/js/static/main.js
+++ Public/Admin_v3/js/static/main.js
@@ -70,8 +70,6 @@ function classicProjectSwiper(){
         watchOverflow: true,
         observer: true,//修改swiper自己或子元素时，自动初始化swiper
         observeParents: true,//修改swiper的父元素时，自动初始化swipe
-        slidesPerView: 4,//一行显示3个
-        slidesPerColumn: 3,//显示2行
         //width: parseInt(slidesOffsetAfterDataInt) + docmentWidth,
         navigation: {
             nextEl: '.swiper-button-next',//自动隐藏
@@ -135,8 +133,6 @@ function otherProjectSwiper(){
         watchOverflow: true,
         observer: true,//修改swiper自己或子元素时，自动初始化swiper
         observeParents: true,//修改swiper的父元素时，自动初始化swipe
-        slidesPerView: 4,//一行显示3个
-        slidesPerColumn: 3,//显示2行
         //width: parseInt(slidesOffsetAfterDataInt) + docmentWidth,
         navigation: {
             nextEl: '.swiper-button-next',//自动隐藏

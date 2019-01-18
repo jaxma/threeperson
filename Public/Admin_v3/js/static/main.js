d56ebe666d773e762c8881b8f91c3bd3d072f1fe
zhang <296684750@qq.com>
2019-01-18 17:15:47 +0800

    背景抖动

--- Public/Admin_v3/js/static/main.js
+++ Public/Admin_v3/js/static/main.js
@@ -22,16 +22,6 @@ $(function(){
     });
 })
 
-if(navigator.userAgent.match(/Trident\/7\./)) {
-    document.body.addEventListener && document.body.addEventListener("mousewheel", function() {
-        event.preventDefault();
-        var wd = event.wheelDelta;
-        var csp = window.pageYOffset;
-        window.scrollTo(0, csp - wd);
-        debugger
-    });
-} 
-
 //点击打开侧边栏
 $(document).on("click","#open-menu-button",function(){
     var main_menu = $("#main-menu");

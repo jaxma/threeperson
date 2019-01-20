c6bfc48f38b8f669fb0b6783dc09941054a95206
zhang <296684750@qq.com>
2019-01-20 14:21:57 +0800

    链接

--- App/Modules/Admin/Action/AboutUsAction.class.php
+++ App/Modules/Admin/Action/AboutUsAction.class.php
@@ -8,6 +8,27 @@ class AboutUsAction extends CommonAction {
     public function index() {
         $this->display();
     }
+    public function news() {
+        $this->display();
+    }
+    public function about() {
+        $this->display();
+    }
+    public function newsdetail() {
+        $this->display();
+    }
+    public function project() {
+        $this->display();
+    }
+    public function projectdetail() {
+        $this->display();
+    }
+    public function search() {
+        $this->display();
+    }
+    public function contact() {
+        $this->display();
+    }
 
     
     // public function wuyao() {

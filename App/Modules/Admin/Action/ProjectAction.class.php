f98cf845b356dc56771bae79818912e4d99777d1
Your name <you@example.com>
2019-01-23 17:24:09 +0800

    .

--- App/Modules/Admin/Action/ProjectAction.class.php
+++ App/Modules/Admin/Action/ProjectAction.class.php
@@ -15,15 +15,22 @@ class ProjectAction extends CommonAction {
         $lang_change = $this->lang==1?0:1;
         $this->lang_url = U('Index/index',array('lang'=>$lang_change));
 
-        $this->project_cat = $this->cat_model->where('status = 1 and pid = 1')->select();
+        $this->cats = $this->cat_model->where('status = 1 and pid = 1')->select();
 
-        $project = $this->project_model->where('classical = 1')->order('sequence desc')->limit(100)->select();
+        if($this->cat_id){
+            $project = $this->project_model->where('cat2 = '.$this->cat_id)->order('sequence desc')->limit(100)->select();
+        }else{
+            $project = $this->project_model->where('classical = 1')->order('sequence desc')->limit(100)->select();
+        }
+
         foreach ($project as $k => $v) {
             $detail = $this->detail_arr($v['detail']);
             $detail_en = $this->detail_arr($v['detail_en']);
             $project[$k]['position'] = $detail[0];
             $project[$k]['position_en'] = $detail_en[0];
         }
+
+        $this->project = $project;
         $this->display();
     }
 

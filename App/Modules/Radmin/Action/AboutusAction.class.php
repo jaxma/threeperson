323521161e7d8539ed885cdf6d4082bdf6d95dfb
wcm company <413866191@qq.com>
2019-01-23 14:12:06 +0800

    wcm modify 2018-1-23

--- App/Modules/Radmin/Action/AboutusAction.class.php
+++ App/Modules/Radmin/Action/AboutusAction.class.php
@@ -67,7 +67,6 @@ class AboutusAction extends CommonAction {
         $content_en = trim(I('post.content_en',''));
         $content_en = $this->formateStr($content_en);
 
-        var_dump($_POST);exit();
         if(empty($title)||empty($title_en)||empty($isopen)||empty($many_image)){
             $this->error('红色带星项目必须填写，请检查后重新提交');
             exit();

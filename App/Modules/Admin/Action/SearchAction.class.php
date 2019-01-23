410c2dc6703a81fb7b6c25c0833e49d94baf5718
wcm <13802540915@163.com>
2019-01-23 22:02:22 +0800

    wcm modify 2019-1-23

--- App/Modules/Admin/Action/SearchAction.class.php
+++ App/Modules/Admin/Action/SearchAction.class.php
@@ -10,8 +10,21 @@ class SearchAction extends CommonAction {
         parent::_initialize();
     }
 
-   
     public function index() {
+
+    	$keyword = I('word','');
+    	if(empty($keyword)) $this->redirect('Index/index',array('lang'=>$this->lang));
+    	$Project_model = $this->project_model;
+    	$News_model = $this->news_model;
+    	$title = $title_en = $content_en = $content_en = $keyword;
+    	if($this->lang){
+    		$plist = $Model->where("isopen=1 and (  title_en like '%%d%' or content_en like '%%f%')",array($title_en,$content_en))->select();
+    	}else{
+    		$plist = $Model->where("isopen=1 and ( title like '%%d%' or content like '%%f%')",array($title,$content))->select();
+    	}
+    	
+    	var_dump($list);
+    	exit();
         $lang_change = $this->lang==1?0:1;
         $this->lang_url = U('Index/index',array('lang'=>$lang_change));
         $this->display();

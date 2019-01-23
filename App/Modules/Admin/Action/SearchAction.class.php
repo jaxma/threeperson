e625e0ad32fa46c22474bb0e717029a21a6f042a
wcm <13802540915@163.com>
2019-01-23 22:03:24 +0800

    wcm modify 2019-1-23

--- App/Modules/Admin/Action/SearchAction.class.php
+++ App/Modules/Admin/Action/SearchAction.class.php
@@ -18,12 +18,12 @@ class SearchAction extends CommonAction {
     	$News_model = $this->news_model;
     	$title = $title_en = $content_en = $content_en = $keyword;
     	if($this->lang){
-    		$plist = $Model->where("isopen=1 and (  title_en like '%%d%' or content_en like '%%f%')",array($title_en,$content_en))->select();
+    		$plist = $Project_model->where("isopen=1 and (  title_en like '%%d%' or content_en like '%%f%')",array($title_en,$content_en))->select();
     	}else{
-    		$plist = $Model->where("isopen=1 and ( title like '%%d%' or content like '%%f%')",array($title,$content))->select();
+    		$plist = $Project_model->where("isopen=1 and ( title like '%%d%' or content like '%%f%')",array($title,$content))->select();
     	}
     	
-    	var_dump($list);
+    	var_dump($plist);
     	exit();
         $lang_change = $this->lang==1?0:1;
         $this->lang_url = U('Index/index',array('lang'=>$lang_change));

3b3e66f898637bcf301233b1fa8af52bcff3a6a3
wcm company <413866191@qq.com>
2019-01-24 15:27:04 +0800

    wcm commit 2019-1-24

--- App/Lib/Action/Admin.class.php
+++ App/Lib/Action/Admin.class.php
@@ -4,99 +4,31 @@ header("Content-Type: text/html; charset=utf-8");
 
 
 class Admin {
-    
     public $is_add_log = TRUE;//是否添加日志
-    
     public $admin_auth = [
         '1' =>  '权限',
-//         '2' =>  '代理查看',
-//         '15' =>  '代理操作',
-//         '13' =>  '代理审核',
-//         '25'    =>  '发展代理',
-        
-//         '5' =>  '虚拟币',
-//         '16' =>  '虚拟币审核',
-        
-//         '3' =>  '代理商城',
-//         '17' =>  '代理商城产品属性',
-//         '18' =>  '代理商城产品属性',
-        
-//         '4' =>  '积分规则',
-//         '19' =>  '积分订单',
-//         '20' =>  '积分订单',
-        
-//         '6' =>  '出库',
-//         '7' =>  '市场营销',
-//         '8' =>  '微官网',
-//         '9' =>  '返利',
-//         '10' =>  '品牌商城',
-// //        '11' =>  '营销活动',
-//         '12'=>  '数据分析',
-//         '14'  => '云仓下单',
-        
-        '21'  => '系统配置',
-        '98'  => '公司内容管理',
-        '97'  => '摄影棚',
-        '99'  => '摄影分类管理',
-        '100'  => '摄影封面管理',
-        '101'  => '视频分享管理',
-        
-        // '22'    =>  '运费模板',
-        // '23'    =>  '防伪管理',
-        // '24'    =>  '消息管理',
-        
-        // '26'    =>  '仓库管理',
+        '2'  => '系统配置',
+        '3'  => '网站分类管理',
+        '4'  => '项目',
+        '5'  => '新闻',
+        '6'  => '关于TOPOS',
     ];
-    
+
     //权限相关对应的module
     public $admin_auth_module = [
         '1' =>  'admin',
-        '2' =>  'manager,upgrade',
-        '15' =>  'manager,regulations,info',
-        '13' =>  'manager',
-        
-        '5' =>  'funds',
-        '16' =>  'funds',
-        
-        '3' =>  'order,inform',
-        '17' =>  'sku',
-        '18' =>  'shipping',
-        
-        '4' =>  'integral',
-        '19' =>  'integralorder',
-        '20' =>  'integraltemplet',
-        
-        '6' =>  'stock',
-        '7' =>  'market',
-        '8' =>  'publicity,aptitude,goods,info',
-        '9' =>  'rebate,newrebate',
-        '10' =>  'malltemplet,mallorder',
-//        '11' =>  'sale',
-        '12'=>  'analysis',
-        '14'  => 'stockorder',
-        
-        '21'  => 'webset',
-        
-        '22'    =>  'shipping',
-        '23'    =>  'security',
-        '24'    =>  'inform',
-        '25'    =>  'templet',
-        '26'    =>  'depot',
-
-        '98'  => 'photo',
-        '97'  => 'photo',
-        '99'  => 'photo',
-        '100'  => 'photo',
-        '101'  => 'video',
+        '2'  => 'webset',
+        '3'  => 'photo',
+        '4'  => 'item',
+        '5'  => 'news',
+        '6'  => 'aboutus',
     ];
-    
     //例外的权限
     //首页、用户手册、发展链接
     public $admin_auth_extra = [
         'index','user','info','admin'
     ];
-    
-    
+
     /**
      * 架构函数
      */

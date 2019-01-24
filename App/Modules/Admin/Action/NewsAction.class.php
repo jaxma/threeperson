e1d107af3350d99c428d4691e6bc208791944ae4
wcm company <413866191@qq.com>
2019-01-24 10:11:43 +0800

    wcm commit 2019-1-24

--- App/Modules/Admin/Action/NewsAction.class.php
+++ App/Modules/Admin/Action/NewsAction.class.php
@@ -10,10 +10,28 @@ class NewsAction extends CommonAction {
         parent::_initialize();
     }
 
-   
     public function index() {
+        $model = $this->news_model;
+        $con = array('isopen'=>1);
+
+        $tmp_list = $model->where($con)->field('id,cat1,cat2,title,title_en,image,publish_time')->order('sequence')->select();
+        $list = array();
+        if(!empty($tmp_list)){
+            foreach ($list as $k => $val) {
+                $tmp = array();
+                $tmp['title'] = $this->lang?htmlspecialchars_decode($val['title_en']):htmlspecialchars_decode($val['title']);
+                $tmp['image'] = $val['image'];
+                $tmp['id'] = $val['id'];
+                $tmp['cat2'] = $val['cat2'];
+                $tmp['cat1'] = $val['cat1'];
+                $tmp['publish_time'] = date("Y-m-d",$val['publish_time']);
+                $list[] = $tmp;
+            }
+        }
+        $this->title = $this->lang?"NEWS":"新闻";
         $lang_change = $this->lang==1?0:1;
-        $this->lang_url = U('Index/index',array('lang'=>$lang_change));
+        $this->lang_url = U('News/index',array('lang'=>$lang_change));
+        $this->list = $list;
         $this->display();
     }
 
